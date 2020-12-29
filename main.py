fe0bc4e7e5f2441f5318d7752e4ab5a262c94b0a
Remoh <remoh007@gmx.de>
2020-12-29 19:39:07 +0100

    Updated main to run solutions.

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 # Main file to run solutions for solved problems
+from importlib import import_module
+import timeit
+
 
 def main():
     print("ProjectEuler Solutions by Remoh007")
+    try:
+        problem = int(input("Enter problem #: "))
+        try:
+            p = import_module("problem.p{}".format(problem))
+            print("Running solution for problem #{}".format(problem))
+            runtime = timeit.timeit(p.solution, number=1)
+            print("Runtime: {} milliseconds".format(runtime*1000))
+        except ModuleNotFoundError:
+            print("There is no solution for problem {}".format(problem))
+    except ValueError:
+        print("You need to enter a valid number.")
 
 
 if __name__ == "__main__":
