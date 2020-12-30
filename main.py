9e0bfc96f8e46d948730ef44aef50b29e0740bd3
Remoh <remoh007@gmx.de>
2020-12-30 20:25:06 +0100

    Solved p2

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ import timeit
 def main():
     print("ProjectEuler Solutions by Remoh007")
     try:
-        problem = int(input("Enter problem #: "))
+        problem = int(input("Enter problem # "))
         try:
             p = import_module("problem.p{}".format(problem))
             print("Running solution for problem #{}".format(problem))
             runtime = timeit.timeit(p.solution, number=1)
             print("Runtime: {} milliseconds".format(runtime*1000))
-        except ModuleNotFoundError:
+        except ModuleNotFoundError as e:
+            print(e)
             print("There is no solution for problem {}".format(problem))
     except ValueError:
         print("You need to enter a valid number.")
