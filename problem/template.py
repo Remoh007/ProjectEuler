7c0e5020bbba29c2da17f6b6de04fcf65d2adb3f
Remoh <remoh007@gmx.de>
2020-12-30 19:12:29 +0100

    Added Problem Template

---
+++ problem/template.py
@@ -0,0 +1,10 @@
+import timeit
+
+
+def solution():
+    pass
+
+
+if __name__ == '__main__':
+    runtime = timeit.timeit(solution, number=1)
+    print("Runtime: {} milliseconds".format(runtime*1000))
